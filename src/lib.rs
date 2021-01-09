85b663c664882a0aaf620ef5f1c9e9c44e5e6069
ambiso <ambiso@invalid>
2021-01-09 20:17:08 +0100

    Add comments

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,17 @@
+// Generational References 
+// Adapted from https://vale.dev/blog/generational-references 
+
 use std::{cell::UnsafeCell, marker::PhantomData, ptr::NonNull};
 
 const MAX_ALLOCS: usize = 1 << 9;
 
 struct GrArenaInternal {
+    // Instead of packing the generational numbers with the allocation,
+    // we use an extra memory region. This way we can avoid having to
+    // use a custom allocator that guarantees that the generational numbers
+    // are never used for anything other than generational numbers.
     gens: Vec<Box<[u64; MAX_ALLOCS]>>,
+    // Free list
     unused: Vec<usize>,
 }
 
@@ -22,10 +30,14 @@ impl GrArena {
     }
 
     pub fn alloc<'a, T>(&'a self, v: T) -> Gr<'a, T> {
+        // Safety:
+        // We don't hand out references to the arena.
+        // Additionally, nobody else can own the arena mutably, since we borrowed it.
         let arena = unsafe { &mut *self.inner.get() };
         loop {
             match (*arena).unused.pop() {
                 Some(gen_idx) => {
+                    // Found an unused slot, return a strong reference to it
                     return Gr {
                         ptr: NonNull::from(Box::leak(Box::new(v))),
                         gen_idx: gen_idx,
@@ -34,6 +46,7 @@ impl GrArena {
                     };
                 }
                 None => {
+                    // Add more slots if we ran out
                     arena.gens.push(Box::new([1; MAX_ALLOCS]));
                     for i in 0..MAX_ALLOCS {
                         arena.unused.push(i + (arena.gens.len()-1) * MAX_ALLOCS);
@@ -45,9 +58,15 @@ impl GrArena {
 }
 
 pub struct Gr<'a, T> {
+    // The contained data
     ptr: NonNull<T>,
+    // The index into the generational numbers array
     gen_idx: usize,
+    // A pointer to the owning arena.
+    // Could be removed if we only had a single global arena.
     arena: *mut GrArenaInternal,
+    // Bind the lifetime of the reference to the lifetime of the generational numbers:
+    // Must not outlive the arena
     phantom: std::marker::PhantomData<&'a u64>,
 }
 
@@ -59,6 +78,7 @@ impl<'a, T> Gr<'a, T> {
     }
 
     pub fn weak(&self) -> Weak<'a, T> {
+        // Get a pointer to the GN
         let gen = unsafe { self.gen() };
         Weak {
             ptr: self.ptr,
@@ -81,14 +101,21 @@ impl<'a, T> Drop for Gr<'a, T> {
 }
 
 pub struct Weak<'a, T> {
+    // The data
     ptr: NonNull<T>,
+    // Unfortunately storing the generational numbers (GNs) separately
+    // also means we need to store the location of the GN of interest.
+    // Furthermore, we need to dereference 2 pointers to get to the data.
     gen: *const u64,
+    // The generational number we expect
     alloc_gen: u64,
+    // Must not outlive the arena
     phantom: std::marker::PhantomData<&'a u64>,
 }
 
 impl<'a, T> Weak<'a, T> {
     pub fn get(&self) -> Option<&T> {
+        // Check if the GNs mismatch
         if unsafe { *self.gen } != self.alloc_gen {
             None
         } else {
@@ -108,10 +135,14 @@ mod tests {
         let r2;
         {
             let s = arena.alloc(String::from("Hello World"));
+            // Weak references work as long as the single owner exists
             r1 = s.weak();
+            assert_eq!(r1.get(), Some(&String::from("Hello World")));
             r2 = s.weak();
+            assert_eq!(r2.get(), Some(&String::from("Hello World")));
         }
 
+        // Once the single owner vanished, the weak references no longer function
         let s = r1.get();
         assert_eq!(s, None);
         let s = r2.get();
@@ -120,28 +151,45 @@ mod tests {
 
     #[test]
     fn many() {
+        // Test that we can allocate and use many things
         let arena = GrArena::new();
         let mut allocs = Vec::new();
 
-        for _ in 0..1500 {
-            allocs.push(arena.alloc(String::from("Hello World")));
-        }
+        for _ in 0..3 {
+            for _ in 0..1500 {
+                allocs.push(arena.alloc(String::from("Hello World")));
+            }
 
-        for i in allocs.iter() {
-            i.weak().get().expect("String should be available");
-        }
+            for i in allocs.iter() {
+                i.weak().get().expect("String should be available");
+            }
 
-        let wr;
-        {
-            let r = allocs.pop().unwrap();
-            wr = r.weak();
-        }
-        assert_eq!(wr.get(), None);
-        let new_s = arena.alloc(String::from("test"));
+            let wr;
+            {
+                let r = allocs.pop().unwrap();
+                wr = r.weak();
+            }
+            // Dropping owning ref should invalidate weak ref
+            assert_eq!(wr.get(), None);
+            let new_s = arena.alloc(String::from("test"));
 
-        assert_eq!(wr.get(), None);
-        let wr2 = new_s.weak();
-        let s = wr2.get();
-        assert_ne!(s, None);
+            assert_eq!(wr.get(), None);
+            let wr2 = new_s.weak();
+            let s = wr2.get();
+            assert_eq!(s, Some(&String::from("test")));
+
+            // Store all weak refs, drop all owning refs, and test that none can be retrieved
+            let mut weak_refs = Vec::new();
+            
+            for or in allocs.iter() {
+                weak_refs.push(or.weak());
+            }
+
+            allocs.clear();
+
+            for wr in weak_refs.iter() {
+                assert_eq!(wr.get(), None);
+            }
+        }
     }
 }
